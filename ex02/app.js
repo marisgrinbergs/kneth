5a8c67a04948183403a0b73a036f42ecb4777cc3
Maris <maris@MacBook-Pro-de-Maris.local>
2020-04-23 18:01:48 +0200

    2

--- ex02/app.js
+++ ex02/app.js
@@ -9,17 +9,11 @@ for (i = 0; i < boutons.length; i++) {
 }
 */
 
-var bouton1 = document.querySelector(".bouton1");
-bouton1.addEventListener("click", clickBouton1);
-
-var bouton2 = document.querySelector(".bouton2");
-bouton2.addEventListener("click", clickBouton2);
-
-var bouton3 = document.querySelector(".bouton3");
-bouton3.addEventListener("click", clickBouton3);
+let key;
+let value;
 
 var oReq = new XMLHttpRequest();
-    oReq.open('GET', "https://maristest1.free.beeceptor.com/my/api/path", true);
+    oReq.open('GET', "https://marislol.free.beeceptor.com/my/api/path", true);
     oReq.send();
 
     oReq.onreadystatechange = function() {
@@ -34,75 +28,38 @@ function setBouton (event)
     function reqListener (event2) {
 
     let req = JSON.parse(event2.target.responseText);
-    object = (Object.keys(req));
+    key = (Object.keys(req));
+    value = (Object.values(req));
 
-    document.querySelector(".bouton1").innerHTML = object[0];
-    document.querySelector(".bouton2").innerHTML = object[1];
-    document.querySelector(".bouton3").innerHTML = object[2];
+    document.querySelector(".bouton1").innerHTML = key[0];
+    document.querySelector(".bouton2").innerHTML = key[1];
+    document.querySelector(".bouton3").innerHTML = key[2];
     }
 
     var oReq = new XMLHttpRequest();
     oReq.onload = reqListener;
-    oReq.open('GET', "https://maristest1.free.beeceptor.com/my/api/path", true);
+    oReq.open('GET', "https://marislol.free.beeceptor.com/my/api/path", true);
     oReq.send();
 }
 
-/*
- function clickBouton (event) {
+function clickBouton (event) {
 
     function reqListener (event2) {
+        console.log("key" + key);
+        console.log("value" + value);
 
-    let req = JSON.parse(event2.target.responseText);
-    object = (Object.values(req));
-    document.querySelector(".bouton1").innerHTML = object[0];
-    document.querySelector(".bouton2").innerHTML = object[1];
-    document.querySelector(".bouton3").innerHTML = object[2];
+        var cle = event.target.innerText;
+        console.log("cle" + cle);
+        var object = JSON.parse(event2.target.responseText);
+        if (key[0] === cle)
+            document.querySelector(".bouton1").innerHTML = value[0];
+        else if (key[1] === cle)
+            document.querySelector(".bouton2").innerHTML = value[1];
+        else if (key[2] === cle)
+            document.querySelector(".bouton3").innerHTML = value[2];
     }
     var oReq = new XMLHttpRequest();
     oReq.onload = reqListener;
-    oReq.open('GET', "https://maristest1.free.beeceptor.com/my/api/path", true);
-    oReq.send();
-}
-*/
-
-function clickBouton1 (event) {
-
-    function reqListener (event2) {
-
-    let req = JSON.parse(event2.target.responseText);
-    object = (Object.values(req));
-    document.querySelector(".bouton1").innerHTML = object[0];
-    }
-    var oReq = new XMLHttpRequest();
-    oReq.onload = reqListener;
-    oReq.open('GET', "https://maristest1.free.beeceptor.com/my/api/path", true);
-    oReq.send();
-}
-
-function clickBouton2 (event) {
-
-    function reqListener (event2) {
-
-    let req = JSON.parse(event2.target.responseText);
-    object = (Object.values(req));
-    document.querySelector(".bouton2").innerHTML = object[1];
-    }
-    var oReq = new XMLHttpRequest();
-    oReq.onload = reqListener;
-    oReq.open('GET', "https://maristest1.free.beeceptor.com/my/api/path", true);
-    oReq.send();
-}
-
-function clickBouton3 (event) {
-
-    function reqListener (event2) {
-
-    let req = JSON.parse(event2.target.responseText);
-    object = (Object.values(req));
-    document.querySelector(".bouton3").innerHTML = object[2];
-    }
-    var oReq = new XMLHttpRequest();
-    oReq.onload = reqListener;
-    oReq.open('GET', "https://maristest1.free.beeceptor.com/my/api/path", true);
+    oReq.open('GET', "https://marislol.free.beeceptor.com/my/api/path", true);
     oReq.send();
 }
